adeca1b5b5d9b7b7853ee51a527f0bd2abd5bf36
Akhrorxuja <akhror0103@gmail.com>
2025-04-22 01:49:05 -0400

    leetcode

---
+++ addition.py
@@ -0,0 +1,23 @@
+
+z = 4
+y = 6
+def add_nums(a, b) -> int:
+    return (a + b)
+
+
+c = 7
+d = 14
+
+
+
+print(add_nums(z, y))
+print(add_nums(c, d))
+
+"""
+given list of numbers, need to print out length of list
+"""
+
+numbers = [123, 4562, 6654, 4443]
+length = len(numbers)
+print(length)
+y = 2
